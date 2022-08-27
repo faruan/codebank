36e730a3c2d19ba25c6e87e0ef0ba9df2117cc8b
faruan <filipe.cezar@gmail.com>
2022-08-27 14:43:14 -0300

    Ajustes no docker-compose

--- infrastructure/grpc/server/server.go
+++ infrastructure/grpc/server/server.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"github.com/faruan/codebank/infrastructure/grpc/pb"
+	"github.com/faruan/codebank/infrastructure/grpc/service"
 	"log"
 	"net"
 	"github.com/faruan/codebank/usecase"
@@ -14,11 +18,15 @@ func NewGRPCServer() GRPCServer {
 	return GRPCServer{}
 }
 
-func (s GRPCServer) Serve() {
+func (g GRPCServer) Serve() {
 	lis, err := net.Listen("tcp","0.0.0.0:50052")
 	if err != nil {
 		log.Fatalf("couldn't listen tpc port")
 	}
-	// transactionService := service.NewTransactionService()
-	
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
 }
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"github.com/faruan/codebank/domain"
+	"github.com/faruan/codebank/infrastructure/grpc/server"
+	"github.com/faruan/codebank/infrastructure/kafka"
 	"github.com/faruan/codebank/infrastructure/repository"
 	"github.com/faruan/codebank/usecase"
 	"log"
@@ -13,27 +14,22 @@ import (
 func main() {
 	db := setupDb()
 	defer db.Close()
-
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Wesley"
-	cc.ExpirationYear = 2021
-	cc.ExpirationMonth = 7
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
-
-	repo := repository.NewTransactionRepositoryDb(db)
-	err := repo.CreateCreditCard(*cc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	serveGrpc(processTransactionUseCase)
 }
 
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDb(db)
 	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
 	return useCase
+}
+
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer("host.docker.internal:9092")
+	return producer
 }
 
 func setupDb() *sql.DB {
@@ -50,3 +46,25 @@ func setupDb() *sql.DB {
 	}
 	return db
 }
+
+func serveGrpc(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	fmt.Println("Rodando gRPC Server")
+	grpcServer.Serve()
+}
+
+// cc := domain.NewCreditCard()
+// 	cc.Number = "1234"
+// 	cc.Name = "Filipe"
+// 	cc.ExpirationYear = 2021
+// 	cc.ExpirationMonth = 7
+// 	cc.CVV = 123
+// 	cc.Limit = 1000
+// 	cc.Balance = 0
+
+// 	repo := repository.NewTransactionRepositoryDb(db)
+// 	err := repo.CreateCreditCard(*cc)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
